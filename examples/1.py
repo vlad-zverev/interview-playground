c7cabc8490cdc46a79917c019714460573689119
vzverev <vzverev@freetechs.org>
2024-06-20 16:40:08 +0400

    few

--- examples/1.py
+++ examples/1.py
@@ -2,7 +2,7 @@ import asyncio
 import time
 
 
-async def run_some_risky(i) -> None:
+async def run(i) -> None:
     await asyncio.sleep(1)
 
     if i == 5:
@@ -13,7 +13,7 @@ async def run_some_risky(i) -> None:
 
 async def main() -> None:
     results = await asyncio.gather(
-        *[run_some_risky(i) for i in range(10)],
+        *[run(i) for i in range(10)],
     )
 
     for i in results:
--- examples/7.py
+++ examples/7.py
@@ -15,6 +15,12 @@ class A(P):
     def __call__(self) -> None:
         print(1)
 
+    def __str__(self) -> str:
+        return "1"
+
+    def __getitem__(self, v) -> None:
+        return v
+
     def __enter__(self) -> int:
         print(1)
         return 1
@@ -22,13 +28,25 @@ class A(P):
     def __exit__(self, *a) -> None:
         print(1)
 
+    def __len__(self) -> int:
+        return 1
+
+    def __bool__(self) -> bool:
+        print(1)
+        return True
+
 
 print(1)
 
 a = A(1)
 
+print(a)
+a()
+print(a[1])
+print(len(a))
+
 with a as b:
     print(b)
-    a()
 
-print(1)
+if a:
+    print(1)
